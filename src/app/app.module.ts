d9ac78ac564026be8b6ffd01413d9260b3ce7c85
IlmarKerner <kernerilmar@gmail.com>
2023-03-05 14:28:37 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderBarComponent } from './header-bar/header-bar.component';
+import { HeadComponent } from './head/head.component';
+import { AboutContainerComponent } from './about-container/about-container.component';
+import { MySkillsComponent } from './my-skills/my-skills.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderBarComponent,
+    HeadComponent,
+    AboutContainerComponent,
+    MySkillsComponent
   ],
   imports: [
     BrowserModule,
